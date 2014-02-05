e6ba77316500a6d67397e0cb51f145c87d2c690a
Gus Hahn-Powell <gushahnpowell@gmail.com>
2014-02-04 22:00:09 -0700

    Removed comments

--- pronunciation/pronunciator.py
+++ pronunciation/pronunciator.py
@@ -283,24 +283,4 @@ if __name__ == "__main__":
 
 
 #import difflib as dl
-#dl.get_close_matches('STANKY',poo.known_words)		
-"""
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-
-def edits1(word):
-   splits     = [(word[:i], word[i:]) for i in range(len(word) + 1)]
-   deletes    = [a + b[1:] for a, b in splits if b]
-   transposes = [a + b[1] + b[0] + b[2:] for a, b in splits if len(b)>1]
-   replaces   = [a + c + b[1:] for a, b in splits for c in alphabet if b]
-   inserts    = [a + c + b     for a, b in splits for c in alphabet]
-   return set(deletes + transposes + replaces + inserts)
-
-def known_edits2(word):
-	return set(e2 for e1 in edits1(word) for e2 in edits1(e1) if e2 in NWORDS)
-
-def known(words): return set(w for w in words if w in NWORDS)
-
-def correct(word):
-	candidates = known([word]) or known(edits1(word)) or known_edits2(word) or [word]
-	return max(candidates, key=NWORDS.get)
-"""
+#dl.get_close_matches('STANKY',poo.known_words)	
