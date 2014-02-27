2d0687a5c2c2a357f3116b5a1f46ecc140225bd8
Gus Hahn-Powell <gushahnpowell@gmail.com>
2014-02-26 22:35:35 -0700

    align.py path changes

--- p2fa/align.py
+++ p2fa/align.py
@@ -236,6 +236,12 @@ def getopt2(name, opts, default = None) :
 
 if __name__ == '__main__':
 	
+	#change to script's directory
+	abspath = os.path.abspath(__file__)
+	dname = os.path.dirname(abspath)
+	os.chdir(dname)
+	print "Alignment script location: {0}".format(os.getcwd())
+	
 	try:
 		opts, args = getopt.getopt(sys.argv[1:], "r:s:e:", ["model="])
 		
