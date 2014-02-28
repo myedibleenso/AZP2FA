a68579d7a36f8d97e7aa00b4e10f20b2a5e74dd2
Gus Hahn-Powell <gushahnpowell@gmail.com>
2014-02-27 23:20:17 -0700

    Added preprocessing option and comments

--- p2fa/align_web.py
+++ p2fa/align_web.py
@@ -18,16 +18,35 @@ def logger(message, log_file=log_file):
 	with open(log_file, 'a') as lg:
 		lg.write("{0}\n".format(message))
 
+def add_noise(f_name):
+	"""
+	Add noise tags
+	"""
+	lines = open(f_name, 'r').readlines()
+	processed_lines = ["{line} {{NS}}\n".format(line=l.strip().upper()) for l in lines]
+	with open(f_name, 'w') as ns:
+		for l in processed_lines:
+			ns.write(l)
+
+
 if __name__ == '__main__':
 	open(log_file, 'w').close() #empty log
-	if len(sys.argv) != 4:
+	if len(sys.argv) < 4:
 		logger("wrong number of arguments provided.  Exiting...")
 		sys.exit()
+
 	wav_file_src = sys.argv[1]
 	transcript_src = sys.argv[2]
 	textgrid_dst = sys.argv[3]
-	
-	#print "\naligning \'{0}\'...({1} out of {2})".format(f, completed, total)
+
+	#test if preprocessing is necessary...
+	if len(sys.argv) == 5:
+		processing_option = sys.argv[4]
+		logger("Processing transcript with option \"{0}\"".format(processing_option))
+		if processing_option == "noise":
+			add_noise(transcript_src)
+
+	#send to aligner...
 	align_command = "python {align_script} {wav_file} {transcript_file} {textgrid_dst}".format(align_script=align_script, wav_file=wav_file_src, transcript_file=transcript_src, textgrid_dst=textgrid_dst)
 	logger("calling align script: \"{0}\"".format(align_command))
 	align_command = shlex.split(align_command)
