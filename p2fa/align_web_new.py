d6f8b7d1383a030bb65ba6fd6600e527f749991f
Gus Hahn-Powell <gushahnpowell@gmail.com>
2014-02-28 10:16:17 -0700

    changes for newer version of AZP2FA web interface

--- p2fa/align_web_new.py
+++ p2fa/align_web_new.py
@@ -8,8 +8,7 @@ import shlex
 called by process.php
 """
 
-align_script = os.path.expanduser("~/github/forced-alignment/AZP2FA/p2fa/align.py")
-log_file = os.path.expanduser("~/websites/poop/uploads/debug_log")
+log_file = os.path.expanduser("~/websites/poop/uploads/debug2_log")
 
 def logger(message, log_file=log_file):
 	"""
@@ -32,21 +31,28 @@ def add_noise(f_name):
 
 
 if __name__ == '__main__':
+	abspath = os.path.abspath(__file__)
+	dname = os.path.dirname(abspath)
+	align_script = os.path.join(dname, "align_new.py")
+	#align_script = os.path.expanduser("~/github/forced-alignment/forced-alignment/p2fa/align.py")
+	
+	#args: wav txt processing_option lang
 	open(log_file, 'w').close() #empty log
-	if len(sys.argv) < 4:
+	logger("align script: {0}".format(align_script))
+	if len(sys.argv) != 6:
 		logger("wrong number of arguments provided.  Exiting...")
 		sys.exit()
 
 	wav_file_src = sys.argv[1]
 	transcript_src = sys.argv[2]
 	textgrid_dst = sys.argv[3]
+	processing_option = sys.argv[4]
+	language = sys.argv[5]
 
 	#test if preprocessing is necessary...
-	if len(sys.argv) == 5:
-		processing_option = sys.argv[4]
-		logger("Processing transcript with option \"{0}\"".format(processing_option))
-		if processing_option == "noise":
-			add_noise(transcript_src)
+	logger("Processing transcript with option \"{0}\"".format(processing_option))
+	if processing_option == "noise":
+		add_noise(transcript_src)
 
 	#send to aligner...
 	align_command = "python {align_script} {wav_file} {transcript_file} {textgrid_dst}".format(align_script=align_script, wav_file=wav_file_src, transcript_file=transcript_src, textgrid_dst=textgrid_dst)
@@ -59,6 +65,7 @@ if __name__ == '__main__':
 	if e:
 		logger("error: {0}".format(e))
 	#textgrid_dst = textgrid_dst.split("poop")[-1]
+	#handle deletion here: $rm_output = shell_exec("rm $audio_dst; rm $transcript_dst");
 	print textgrid_dst
 
 
